1936b6107088606d832303e1295f468ef71ed997
Patrick Hackman <pajohack@gmail.com>
2024-04-05 13:18:07 +0000

    Add check answer function

--- run.py
+++ run.py
@@ -5,12 +5,22 @@ import random
 
 def format_data(account):
     """
-    Format the account data into printable format.
+    Takes the account data and returns printable format.
     """
     account_name = account["name"]
     account_descr = account["description"]
     account_country = account["country"]
     return f"{account_name}, a {account_descr}, from {account_country}"
+
+def check_answer(guess, a_followers, b_followers):
+    """
+    Take the user guess and follower counts and
+    returns if they got it right.
+    """
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
 
 
 print(logo)
@@ -21,4 +31,12 @@ if account_a == account_b:
     account_b = random.choice(data)
 
 print(f"Compare A: {format_data(account_a)}.")
-print(f"Compare B: {format_data(account_b)}.")
+print(vs)
+print(f"Against B: {format_data(account_b)}.")
+
+guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+a_follower_count = account_a["follower_count"]
+b_follower_count = account_b["follower_count"]
+
+is_correct = check_answer(guess, a_follower_count, b_follower_count)
